f38c671b675fb2baa85f53c40d855e8a26da9afc
m-ghalandari <mohammadghalandari1998@gmail.com>
2025-07-22 18:27:38 +0200

    test for git credential manager

--- auth-service/src/main/java/de/smartgrow/authservice/dto/LoginRequestDTO.java
+++ auth-service/src/main/java/de/smartgrow/authservice/dto/LoginRequestDTO.java
@@ -3,7 +3,7 @@ package de.smartgrow.authservice.dto;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
-
+//test for git credential manager
 public class LoginRequestDTO {
     @NotBlank(message = "Email is required")
     @Email(message = "Email should be a valid email address")
